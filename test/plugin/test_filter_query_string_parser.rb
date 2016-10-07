26460c362bb6b2a11e17ad7ab8a0378118c307c0
yudppp <yu.d.ppp@gmail.com>
2016-10-07 19:57:45 +0900

    Add json_value params

--- test/plugin/test_filter_query_string_parser.rb
+++ test/plugin/test_filter_query_string_parser.rb
@@ -45,6 +45,24 @@ class QueryStringParserFilterTest < Test::Unit::TestCase
     assert_equal "fuga", data["parsed"]["hoge"]
   end
 
+  def test_filter_json_value_field
+    config = %[
+      key_name query
+      json_value_field parsed
+    ]
+
+    d1 = create_driver(config, "test.no.change")
+    d1.run do
+      d1.filter({ "query" => "foo=bar&hoge=fuga" }, @time)
+    end
+    filtered = d1.filtered_as_array
+
+    data = filtered[0][2]
+    assert_equal "foo=bar&hoge=fuga", data["query"]
+    assert_equal "bar", JSON.parse(data["parsed"])["foo"]
+    assert_equal "fuga", JSON.parse(data["parsed"])["hoge"]
+  end
+
   def test_filter_inject_key_prefix
     config = %[
       key_name query
